6cbd1e75cdbede63ee57497653ec4c0940ce751c
greenmallow <dmarmion08@gmail.com>
2021-04-28 16:05:56 +1000

    Add skeleton Tile class structure

--- Tile.cpp
+++ Tile.cpp
@@ -1,4 +1,41 @@
-
 #include "Tile.h"
 
-// Empty... for now?
+Tile::Tile(Colour colour, Shape shape) {
+    // TODO
+}
+
+Tile::Tile(std::string tileString) {
+    // TODO
+}
+
+Tile::Tile(Tile& other) {
+    // TODO
+}
+
+Tile::Tile(Tile&& other) {
+    // TODO
+}
+
+Tile::~Tile() {
+    // TODO
+}
+
+Colour getColour() {
+    // TODO
+}
+
+void Tile::setColour(Colour newColour) {
+    // TODO
+}
+
+Shape Tile::getShape() {
+    // TODO
+}
+
+void Tile::setShape(Shape newShape)  {
+    // TODO
+}
+
+std::string Tile::toString()  {
+    // TODO
+}
--- Tile.h
+++ Tile.h
@@ -1,6 +1,7 @@
-
 #ifndef ASSIGN2_TILE_H
 #define ASSIGN2_TILE_H
+
+#include <string>
 
 // Define a Colour type
 typedef char Colour;
@@ -10,6 +11,25 @@ typedef int Shape;
 
 class Tile {
 public:
+    // Constructors & destructor
+    Tile(Colour colour, Shape shape);
+    Tile(Tile& other);
+    Tile(Tile&& other);
+    ~Tile();
+
+    // Alternate constructor to parse colour & shape from a string
+    Tile(std::string tileString);
+
+    // Getters & setters
+    Colour getColour();
+    void setColour(Colour newColour);
+    Shape getShape();
+    void setShape(Shape newShape);
+
+    // Get tile in [letter][number] string format
+    std::string toString();
+
+private:
    Colour colour;
    Shape  shape;
 };
