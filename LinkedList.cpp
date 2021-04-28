127ea0cc8ec8f86c168a1600544b6ec5c347962e
RichardForsey89 <s3857811@student.rmit.edu.au>
2021-04-28 16:25:57 +1000

    Add start of LinkedList implementation

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -3,9 +3,63 @@
 
 LinkedList::LinkedList() {
    head = nullptr;
-
-   // TODO
+   size = 0;
 }
 
 LinkedList::~LinkedList() {
+   // Will need to be a for loop that goes through the list until size, deleting
+   // nodes.
+}
+
+void LinkedList::printList(Node* n){
+    while (n != NULL) {
+        std::cout << n->tile << " ";
+        n = n->next;
+    }
+}
+
+void LinkedList::addFront(Tile* tile){
+   if (head == nullptr){
+      Node* newNode = new Node(tile,nullptr);
+      head = newNode;
+      size++;
+   }
+   else {
+      Node* newNode = new Node(tile, head);
+      head = newNode;
+      size++;
+   }
+}
+
+void LinkedList::insert(Tile* tile, int index){
+   Node* n = head;
+   Node* previous = nullptr;
+   int counter = 0;
+
+   while (n != nullptr && counter < index) {
+      previous = n;
+      n = n->next;
+   }
+
+   if (head == nullptr){
+      std::cout << "list was empty, creating first node\n";
+      head = new Node (tile, nullptr);
+   }
+
+   else if(n == nullptr)
+   {
+      std::cout << "index was outside of list size, will add to end\n";
+      Node* newNode = new Node(tile,nullptr);
+      n = newNode;
+      size++;
+   }
+   else{
+      std::cout << "index was inside list size, will add to index "<< 
+      counter << "\n";
+      Node* newNode = new Node(tile,n);
+      previous->next = newNode;
+      n = newNode;
+      size++;
+   }
+   
 }
--- LinkedList.h
+++ LinkedList.h
@@ -2,6 +2,7 @@
 #ifndef ASSIGN2_LINKEDLIST_H
 #define ASSIGN2_LINKEDLIST_H
 
+#include <iostream>
 #include "Node.h"
 
 class LinkedList {
@@ -10,8 +11,26 @@ public:
    LinkedList();
    ~LinkedList();
 
+   void printList(Node* n);
+
+// we won't need addFront or insert, but do them for practise
+   void addFront(Tile* tile);
+   void insert(Tile* tile, int index);
+   void addBack(Tile* tile);
+
+   void removeFront();
+   void remove(int index);
+   void removeBack();
+
+   Tile* getFront();
+   Tile* get(int index);
+   Tile* getBack();
+
+   int findTileIndex(Tile* tile);
+
 private:
    Node* head;
+   int size;
 };
 
 #endif // ASSIGN2_LINKEDLIST_H
