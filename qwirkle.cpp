dbfe40245b800cad6349c8b53524007e7e7aa12d
[Ahmad Farighi s3842662] <s3842662@student.rmit.edu.au>
2021-05-06 03:30:49 +1000

    Remove multiple returns and add miscellaneous methods required by menu

--- qwirkle.cpp
+++ qwirkle.cpp
@@ -20,13 +20,17 @@ void startNewGame();
 bool loadGame();
 void showCredits();
 void terminateGame();
-
+// returns user input as std::string which can be parsed as int, where required, using string stream (as demonstrated in menu)
 string promptUser() {
    string input = "";
    cout << "> ";
    cin >> input;
    return input;
 }
+
+// maybe also have a method called like conductGame(Game* g) which takes in a game `state`/instance and conducts the gameplay loop
+// i.e startnewGame calls Game constructor and then calls conductGame() with the newly created Game object. 
+// loadGame reads file and instantiates Game object with the deserialized contents which is then passed to conductGame().
 
 void startNewGame() {
    cout << "Starting a New Game!" << endl;
@@ -37,50 +41,60 @@ bool loadGame() {
    return false;
 }
 
-void terminateGame() {
+void terminationMessage() {
    cout << "Goodbye" << endl;
 }
+// bit ugly but who cares its just credits ¯\_(ツ)_/¯
+void showCredits() {
+   cout << "-------------------------------------" << endl;
+   cout << "Name: Ahmad Seiam Farighi" << endl << "Student ID: s3842662" << endl
+   << "Email: s3842662@student.rmit.edu.au" << endl << endl;
 
+   cout << "Name: Daniel Marmion" << endl << "Student ID: s3842912" << endl
+   << "Email: s3842912@student.rmit.edu.au" << endl << endl;
 
+   cout << "Name: Richard Forsey" << endl << "Student ID: s3857811" << endl
+   << "Email: s3857811@student.rmit.edu.au" << endl << endl;
+
+   cout << "Name: Aaron Fisher" << endl << "Student ID: s3840619" << endl
+   << "Email: s3840619@student.rmit.edu.au" << endl;
+   cout << "-------------------------------------" << endl << endl;
+}
 
 int main(void) {
    cout << "Welcome to Quirkle!" << endl << "-------------------" << endl;
-   atexit (terminateGame);
-   while(true) {
-         cout << "Menu" << endl << "----" << endl;
-         cout << "1. New Game" << endl;
-         cout << "2. Load Game" << endl;
-         cout << "3. Credits (Show student information)" << endl;
-         cout << "4. Quit" << endl;
-         
-         istringstream iss (promptUser());
-         int selection = 0;
-         iss >> selection;
-         if (iss.fail()) {
-            if (iss.eof()) {
-               exit (EXIT_SUCCESS);
-            }
-            cout << "Invalid Input. Please enter a number from 1-4." << endl;
+   atexit (terminationMessage);
+   bool shouldDisplayMenu = true;
+   do {
+      cout << "Menu" << endl << "----" << endl;
+      cout << "1. New Game" << endl;
+      cout << "2. Load Game" << endl;
+      cout << "3. Credits (Show student information)" << endl;
+      cout << "4. Quit" << endl;
+      
+      istringstream iss (promptUser());
+      int selection = 0;
+      iss >> selection;
+      if (iss.fail()) {
+         if (iss.eof()) {
+            shouldDisplayMenu = false;
+         } else { cout << "Invalid Input. Please enter a number from 1-4." << endl;}
+      } else {
+         if (selection == 1) {
+            startNewGame();
+            shouldDisplayMenu = false;       // assumes menu should not repeat after game is completed (incorrect assumption)
+         } else if (selection == 2) {
+            loadGame();
+            shouldDisplayMenu = false;       // assumes menu should not repeat after game is completed (incorrect assumption)
+         } else if (selection == 3) {
+            showCredits();
+         } else if (selection == 4) {
+            shouldDisplayMenu = false;
          } else {
-            if (selection == 1) {
-               startNewGame();
-            } else if (selection == 2) {
-               loadGame();
-            } else if (selection == 3) {
-               cout << "Showing credits" << endl;
-            } else if (selection == 4) {
-               return EXIT_SUCCESS;
-            } else {
-               cout << "Sorry, that isn't an option. Please enter a number from 1-4" << endl;
-            }
+            cout << "Sorry, that isn't an option. Please enter a number from 1-4" << endl;
          }
-   }
-         
-
-
-
-   
-
+      }
+   } while(shouldDisplayMenu);
 
    LinkedList* list = new LinkedList();
    delete list;
