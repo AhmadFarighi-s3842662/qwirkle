4cb933c135f1312d56eb08bcd21a97aa388753c2
greenmallow <dmarmion08@gmail.com>
2021-05-08 15:28:10 +1000

    Restore qwirkle.cpp to skeleton

--- qwirkle.cpp
+++ qwirkle.cpp
@@ -1,47 +1,9 @@
-
-#include "LinkedList.h"
-
 #include <iostream>
 
-#define EXIT_SUCCESS    0
-
-void testLinkedListFunction();
+#define EXIT_SUCCESS 0
 
 int main(void) {
-   testLinkedListFunction();
-
    std::cout << "TODO: Implement Qwirkle!" << std::endl;
 
-   std::cout << "Richard messing around!" << std::endl;
-
    return EXIT_SUCCESS;
-}
-
-void testLinkedListFunction(){
-   LinkedList* list = new LinkedList();
-   Tile* r1 = new Tile('R',1);
-   Tile* r2 = new Tile('R',2);
-   Tile* r3 = new Tile('R',3);
-   Tile* r4 = new Tile('R',4);
-   Tile* r5 = new Tile('R',5);
-   Tile* r6 = new Tile('R',6);
-   list->addFront(r3);
-   list->addFront(r2);
-   list->addFront(r1);
-   list->addBack(r5);
-   list->addBack(r6);
-   list->insert(r4,3);
-   list->printList();
-   list->remove(1);
-   list->printList();
-   list->removeBack();
-   list->removeFront();
-   list->printList();
-   delete r1;
-   delete r2;
-   delete r3;
-   delete r4;
-   delete r5;
-   delete r6;
-   delete list;
 }
