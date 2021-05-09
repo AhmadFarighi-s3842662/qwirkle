75f1fba724cccc0bd9de44b0609ad9e9b73d19ed
Richard.Forsey89 <62091812+RichardForsey89@users.noreply.github.com>
2021-05-09 14:09:42 +1000

    Fix constructor, copier, deconstructor

--- Node.cpp
+++ Node.cpp
@@ -3,14 +3,18 @@
 
 Node::Node(Tile* pTile, Node* pPrevious, Node* pNext)
 {
-   tile = pTile;
-   previous = pPrevious;
-   next = pNext;
+    tile = new Tile(*pTile);
+    previous = pPrevious;
+    next = pNext;
 }
 
 Node::Node(Node& other)
 {
-   tile = other.tile;
-   previous = other.previous;
-   next = other.next;
+    tile = new Tile(*other.tile);
+    previous = other.previous;
+    next = other.next;
+}
+
+Node::~Node(){
+    delete tile;
 }
